5da32c5413b779129c8601894425f80d7902fdb7
Samspudz <adammarshall11@yahoo.com>
2023-02-16 21:00:14 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Rectangle
+    {
+        private int length;
+        private int height;
+
+        public int Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public int Height
+        {
+            get => height;
+            set => height = value;
+        }
+    }
+
+    public class Utility
+    {
+        static void Area(Rectangle rect) 
+        {
+
+        }
+
+        static void Perimiter(Rectangle rect)
+        {
+
+        }
+    }
+}
